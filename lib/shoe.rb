9070a43a1150f68a0fafdaa96dcae36903b2721e
melia miller <meliajm@gmail.com>
2019-12-13 01:13:50 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,8 +1,12 @@
 # Make your shoe class here!
 
 class Shoe 
-  
-  
   attr_reader :brand
   attr_accessor :color, :size, :material, :condition
+  
+  def initialize(brand)
+    @brand = brand
+  end 
+  
+  
 end 
